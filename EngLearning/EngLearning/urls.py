cde2ea7c0f41bcd752aa282a797e497437cf902c
Dinh Thai <thaithai120724@gmail.com>
2024-10-22 20:28:45 +0700

    code

--- EngLearning/EngLearning/urls.py
+++ EngLearning/EngLearning/urls.py
@@ -20,5 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('courses.urls')),
-    path('paypal/', include('paypal.standard.ipn.urls')),
 ]
